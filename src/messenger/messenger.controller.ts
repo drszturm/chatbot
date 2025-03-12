52e914e0ca78110b522c1c3eee19724ce8e55b6f
dszturm <szturm.david@gmail.com>
2025-03-12 14:07:28 -0300

    fc

---
+++ src/messenger/messenger.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { MessengerService } from './messenger.service';
+import { CreateMessengerDto } from './dto/create-messenger.dto';
+import { UpdateMessengerDto } from './dto/update-messenger.dto';
+import  {Twilio } from 'twilio';
+@Controller('messenger')
+export class MessengerController {
+  constructor(private readonly messengerService: MessengerService,private twilio:Twilio){}
+
+  @Post("/twilio")
+  create(@Body() createMessengerDto: CreateMessengerDto) {
+
+    return this.messengerService.create(createMessengerDto);
+    
+  }
+
+  @Get()
+  findAll() {
+    return this.messengerService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.messengerService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateMessengerDto: UpdateMessengerDto) {
+    return this.messengerService.update(+id, updateMessengerDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.messengerService.remove(+id);
+  }
+}
